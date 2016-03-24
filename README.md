7f52ed25e5a45995bd14ada9e176f47b3b058b85
Benjamin Isberner <b.isberner@sensory-minds.com>
2016-03-24 09:09:55 +0100

    initial git

---
+++ README.md
@@ -0,0 +1,8 @@
+Random user friendly password generator for node.js
+===================================================
+ 
+#### How to use:
+```javascript
+var friendlyPassword = require( 'readable-password' ).randomString( 8, 2 );
+// -> SazuQaY19
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+exports.randomString = function (len, numLen) {
+	var pickRandom = (function pickRandom(array) {
+		return array[Math.floor( Math.random() * array.length )];
+	});
+
+	if (len == undefined) len = 6;
+	if (numLen == undefined) numLen = 2;
+
+	var c    = true;
+	var pass = "";
+
+	// @formatter:off
+	var validCon = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+	var validVoc = ['a', 'e', 'i', 'o', 'u'];
+	var validNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	// @formatter:on
+
+	for (var i = 0; i <= len - numLen; ++i) {
+		if (c)
+			pass += pickRandom( validCon );
+		else
+			pass += pickRandom( validVoc );
+
+		c = !c;
+	}
+
+	for (var n = 0; n < numLen; ++n) {
+		pass += pickRandom( validNum );
+	}
+
+	return pass;
+};
